52daa5c90aa6f2ec0a11a04c552d449a5aba87da
Myumyun Ismail <acesinexile@gmail.com>
2014-11-09 01:39:39 +0200

    Rolling ball logic

---
+++ Assets/Sample Assets/2D/Scripts/RollingObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingObject : MonoBehaviour
+{
+
+
+   
+    public float sphereX = -10f;
+    public float sphereY = 0f;
+    public DestroyableObject wall;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(sphereX * Time.deltaTime, sphereY * Time.deltaTime, 0f));
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Breakable Wall")
+        {
+            Destroy(wall.gameObject);
+        }
+    }
+}
